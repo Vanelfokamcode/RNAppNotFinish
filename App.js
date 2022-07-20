bd8e09971e5e7c4cd4d99286b168a663a6a9ffbb
Vanel FOKAM <108558185+Vanelfokamcode@users.noreply.github.com>
2022-07-20 22:27:05 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { StyleSheet, View, Button, TextInput, FlatList} from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function addGoalHandler(enteredGoalText){
+    setCourseGoals((currentCourseGoals) => [
+        ...currentCourseGoals, 
+        {text: enteredGoalText, id: Math.random().toString() },
+      ]);
+  }
+
+  function deleteGoalHandler(id) {
+    setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+  }
+
+return (
+  <View style={styles.appContainer}>
+    <GoalInput OnAddGoal={addGoalHandler} />
+    <View style={styles.goalsContainer}>
+     <FlatList 
+       data={courseGoals} 
+       renderItem={(itemData) => {
+         return <GoalItem 
+                  text={itemData.item.text} 
+                  id={itemData.id}
+                  onDeleteItem={deleteGoalHandler} />;
+       }} 
+       keyExtractor={(item, index) => {
+          return item.id;
+       }}
+       alwaysBounceVertical={false}
+      />
+        
+    </View>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer:{
+    flex:1,
+    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 16
+  },
+  goalsContainer:{
+    flex: 5
+  }
+});
